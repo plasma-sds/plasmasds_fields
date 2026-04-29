3101ca5e8bc28fb5f2bb8930a9c8cd6f37869c1e
asztalosors <asztalos@reak.bme.hu>
2026-04-29 13:37:03 +0200

    #2 Adding surface loading sequence from etree.

--- w7x/profiles.py
+++ w7x/profiles.py
@@ -1,4 +1,5 @@
 import numpy as np
+import xml.etree.ElementTree as ET
 
 class Points(object):
     def __init__(self, x1, x2, x3):
@@ -24,3 +25,32 @@ class FluxSurface(object):
         self.density = value
         if assymetric_island_density is not None:
             self.assymetric_island_density = assymetric_island_density
+
+def load_w7x_flux_surfaces(filename):
+    """
+    This function loads a W7-X flux surface stored in an xml file.
+    """
+    tree = ET.parse(filename)
+    root = tree.getroot()  # {fltracer.gsoap.boz.hgw.ipp.mpg.de}Result {}
+    surfaces = list()
+
+    for surf in root:
+        phi0 = None
+        x1 = list()
+        x2 = list()
+        x3 = list()
+        for points in surf:
+            if 'points' in points.tag:
+                for point in points:
+                    if 'x1' in point.tag:
+                        x1.append(float(point.text))
+                    elif 'x2' in point.tag:
+                        x2.append(float(point.text))
+                    elif 'x3' in point.tag:
+                        x3.append(float(point.text))
+            elif phi0 is None and 'phi0' in points.tag:
+                phi0 = float(points.text)
+        surface = FluxSurface(x1, x2, x3, phi0)
+        surfaces.append(surface)
+
+    return surfaces
