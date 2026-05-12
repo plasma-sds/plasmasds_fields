016c10514782d3d2f1e48b9a9d2deb4e08f31c70
Akos001225 <akosk20001225@gmail.com>
2026-05-12 15:04:51 +0200

    #13 Filter function to identify island flux surfaces by polinomfit, new ploting function, new Range class to extract data ranges

--- w7x/poincare.py
+++ w7x/poincare.py
@@ -17,14 +17,16 @@ class Points(object):
 
         Parameters
         ----------
-        x1, x2, x3 : array_like
+        x1, x2, x3 , r, z: array_like
             Sequences (or arrays) of equal length holding the first, second
             and third coordinate of each point. They are converted to
             ``numpy.ndarray`` on assignment.
         """
-        self.x1 = np.array(x1)
-        self.x2 = np.array(x2)
-        self.x3 = np.array(x3)
+        self.x1 = np.array(x1)                      # first coordinate
+        self.x2 = np.array(x2)                      # second coordinate
+        self.x3 = np.array(x3)                      # third coordinate
+        self.r = np.sqrt(self.x1**2 + self.x2**2)   # radial distance
+        self.z = self.x3                            # vertical coord
 
     def __array__(self, dtype=None):
         """
@@ -60,7 +62,8 @@ class FluxSurface(object):
     downstream models.
     """
 
-    def __init__(self, x1, x2, x3, phi0, density=0, assymetric_island_density=0):
+    def __init__(self, x1, x2, x3, phi0, 
+                 density=0, assymetric_island_density=0):
         """
         Initialize a flux surface.
 
@@ -76,11 +79,13 @@ class FluxSurface(object):
         assymetric_island_density : float, optional
             Additional density contribution from an asymmetric magnetic
             island, if any. Defaults to 0.
+        
         """
         self.points = Points(x1, x2, x3)
         self.phi0 = phi0
         self.density = density
         self.assymetric_island_density = assymetric_island_density
+        self.N = np.shape(np.asarray(self.points))[1]
 
     def update_density(self, value, assymetric_island_density=None):
         """
@@ -97,6 +102,52 @@ class FluxSurface(object):
         self.density = value
         if assymetric_island_density is not None:
             self.assymetric_island_density = assymetric_island_density
+
+class Range(object):
+    """ 
+    Used to extract the range of different data types in a list of FluxSurface
+    objects or FluxSurface.Points objects:
+        x1, x2, x3, r, z, density
+        
+        Attributes can give back the individual boundaries, or the list object
+        of the range.
+    """
+    
+    def __init__(self, flt):
+        """
+        By creating the Range object, the attributes calculated automatically
+        during the initialisation.
+
+        Parameters
+        ----------
+        flt : list of FluxSurface objects
+            Filtered list of FluxSurface objects.
+
+        Returns
+        -------
+        None.
+
+        """
+        
+        self.x1Min = min([np.min(surf.points.x1) for surf in flt])
+        self.x1Max = max([np.max(surf.points.x1) for surf in flt])
+        self.x2Min = min([np.min(surf.points.x2) for surf in flt])
+        self.x2Max = max([np.max(surf.points.x2) for surf in flt])
+        self.x3Min = min([np.min(surf.points.x3) for surf in flt])
+        self.x3Max = max([np.max(surf.points.x3) for surf in flt])
+        self.rMin  = min([np.min(surf.points.r ) for surf in flt])
+        self.rMax  = max([np.max(surf.points.r ) for surf in flt])
+        self.dMin  = min([np.min(surf.density  ) for surf in flt])
+        self.dMax  = max([np.max(surf.density  ) for surf in flt])
+        self.zMin  = self.x3Min
+        self.zMax  = self.x3Max
+        
+        self.x1 = [self.x1Min, self.x1Max]
+        self.x2 = [self.x2Min, self.x2Max]
+        self.x3 = [self.x3Min, self.x3Max]
+        self.r  = [self.rMin , self.rMax ]
+        self.d  = [self.dMin , self.dMax ]
+        self.z  = [self.zMin , self.zMax ]
 
 def load_w7x_flux_surfaces(filename):
     """
@@ -168,7 +219,10 @@ def box_plot_coordinates(r_min, z_min, r_max, z_max):
     z = np.array([z_min, z_min, z_max, z_max, z_min])
     return r, z
 
-def plot_w7x_flux_surfaces(surfaces, magnetic_conf='', r_range=None, z_range=None, phi=np.nan, boxes=None, aspect=False, save_image=False, legend=False):
+def plot_w7x_flux_surfaces(surfaces, magnetic_conf='', 
+                           r_range=None, z_range=None, phi=np.nan, 
+                           boxes=None, aspect=False, 
+                           save_image=False, legend=False):
     """
     Plot a Poincaré section of W7-X flux surfaces in the (R, z) plane.
 
@@ -202,16 +256,18 @@ def plot_w7x_flux_surfaces(surfaces, magnetic_conf='', r_range=None, z_range=Non
     if not isinstance(surfaces, list):
         surfaces = surfaces.poincare_res.surfs
 
-    colors = ["red", "orange", "yellow", "green", "blue", "purple", "indigo", "violet"]
+    colors = ["red", "orange", "yellow", "green", "blue",
+              "purple", "indigo", "violet"]
     fig, ax = plt.subplots()
 
     num_surfaces = len(surfaces)
 
     for i, surface in enumerate(surfaces):
         if surface.points.x1 is not None and len(surface.points.x1) > 0:
-            r = np.sqrt(np.array(surface.points.x1)**2 + np.array(surface.points.x2)**2)
-            z = np.array(surface.points.x3)
-            ax.scatter(r, z, color=colors[i % 8], s=0.2, label="surface {}".format(i))
+            r = surface.points.r
+            z = surface.points.z
+            ax.scatter(r, z, color=colors[i % 8], s=0.2, 
+                       label="surface {}".format(i))
         else:
             print("Surface {} contains no points!".format(i + 1))
 
@@ -229,7 +285,8 @@ def plot_w7x_flux_surfaces(surfaces, magnetic_conf='', r_range=None, z_range=Non
 
     if aspect:
         ax.set_aspect('equal')
-    ax.set_title("Config: {}, toroidal angle: {:3.2f} rad, {:3.2f} deg.".format(magnetic_conf, phi, phi/np.pi*180.))
+    ax.set_title(("Config: {}, toroidal angle: {:3.2f} rad, {:3.2f} deg."
+                  ).format(magnetic_conf, phi, phi/np.pi*180.))
 
     if boxes:
         for box in boxes:
@@ -298,8 +355,8 @@ def filter_surfaces_by_range(flt, surf_range=None, r_range=None, z_range=None):
     for surf in surfs:
         if type(surf.points.x1) != type(None) and len(surf.points.x1) > 0:
             # Calculate R and Z coordinates
-            r_temp = np.sqrt(np.array(surf.points.x1)**2 + np.array(surf.points.x2)**2)
-            z_temp = np.array(surf.points.x3)
+            r_temp = surf.points.r
+            z_temp = surf.points.z
             
             # Create masks for filtering
             if r_range is not None:
@@ -315,7 +372,8 @@ def filter_surfaces_by_range(flt, surf_range=None, r_range=None, z_range=None):
             # Keep only points within both ranges
             mask = r_mask & z_mask
             
-            # Only include surface if it has at least one point within the ranges
+            # Only include surface if it has at least one point
+            # within the ranges
             if np.any(mask):
                 # Filter the points
                 x1_filtered = np.array(surf.points.x1)[mask].tolist()
@@ -323,7 +381,195 @@ def filter_surfaces_by_range(flt, surf_range=None, r_range=None, z_range=None):
                 x3_filtered = np.array(surf.points.x3)[mask].tolist()
                 
                 # Create new filtered surface
-                filtered_surf = FluxSurface(x1_filtered, x2_filtered, x3_filtered, surf.phi0, surf.density, surf.assymetric_island_density)
+                filtered_surf = FluxSurface(
+                    x1_filtered, x2_filtered, x3_filtered, surf.phi0,
+                    surf.density, surf.assymetric_island_density)
                 filtered_surfs.append(filtered_surf)
     
     return filtered_surfs
+
+def filter_surfaces_by_polyfit(flt, limit_error = 0.015,
+                               limit_number = 100, order = 2):
+    """
+    Filter flux surfaces and their points by polyfit. Used to differentiate
+    island flux surfaces. The method includes a polinom fit (ideally second
+    order), which error can indicate the flux surface relevance. The 0 order
+    coefficient can be also used to estimate the radial position of the 
+    flux surface.
+
+    The input may be either a list of :class:`FluxSurface` instances (as
+    returned by :func:`load_w7x_flux_surfaces`) or a field-line-tracer
+    result object exposing ``poincare_res.surfs``. The function first
+    selects a contiguous slice of surfaces (``surf_range``), then for each
+    remaining surface keeps only the points whose cylindrical
+    ``R = sqrt(x1**2 + x2**2)`` and ``z = x3`` fall within the supplied
+    ranges. Surfaces left with no points are dropped.
+
+    Parameters
+    ----------
+    flt : list of FluxSurface or field-line-tracer result
+        Source surfaces to filter.
+    limit_error : float, optional
+        Limit defined by the error of the polynomfit. 
+        The default is 0.015.
+    limit_number : float, optional
+        Limit defined by the number of points in the FluxSurface object.
+        The default is 100.
+    order : integer, optional
+        order of the polynom fit function. The default is 2.
+
+    Returns
+    ------- , , 
+    surfaces : list of FluxSurface
+        Returns the list of flux surfaces, splitting island surfaces into 
+        low field and high field side.
+    types : np.array with the shape of len(surfaces)
+        Containing the type of each flux surface: not island (0), low field
+        side (1) and high field side (2).
+    errors : np.array with the shape of len(surfaces)
+        Contains the error of the polinom fit for each flux surface.
+    coeffs : np.array with the shape of (len(surfaces), order)
+        Contains the coefficients of each fit.
+    """
+    
+    types = list()
+    coeffs = list()
+    errors = list()
+    Surfs = list()
+    
+    for i, surf in enumerate(flt):
+        r, z = surf.points.r, surf.points.z
+        coeff = np.polyfit(z[:], r[:], order)
+        error = np.sqrt(np.mean((r - np.polyval(coeff, z))**2))
+        
+        # Conditions to differentiate island and not-island surfaces:
+        # Error is high | or | the number of points are large
+        condition_1 = error > limit_error
+        condition_2 = surf.N > limit_number
+        if (condition_1 or condition_2):
+            # island case: split surface to low and high field side
+            p = np.polyval(coeff, z)    # center line by polyfit
+            o = r-p > 0                 #  low field side point filter
+            i = np.invert(o)            # high field side point filter
+            
+            # LOW FIELD SIDE
+            surf_o = FluxSurface(surf.points.x1[o], 
+                                 surf.points.x2[o],
+                                 surf.points.x3[o],
+                                 surf.phi0, density=surf.density)
+            coeff = np.polyfit(z[o], r[o], order)
+            error = np.sqrt(np.mean((r - np.polyval(coeff, z))**2))
+            
+            Surfs.append(surf_o)
+            coeffs.append(coeff)
+            errors.append(error)
+            types.append(2)
+            
+            # HIGH FIELD SIDE
+            surf_i = FluxSurface(surf.points.x1[i], 
+                                 surf.points.x2[i],
+                                 surf.points.x3[i],
+                                 surf.phi0, density=surf.density)
+            coeff = np.polyfit(z[i], r[i], order)
+            error = np.sqrt(np.mean((r - np.polyval(coeff, z))**2))
+            
+            Surfs.append(surf_i)
+            coeffs.append(coeff)
+            errors.append(error)
+            types.append(1)
+            
+        else:
+            # not-island case
+            # Leave all surfaces as they are.
+            Surfs.append(surf)
+            coeffs.append(coeff)
+            errors.append(error)
+            types.append(0)
+        
+    return Surfs, np.array(types), np.array(errors), np.asarray(coeffs)
+
+def plot_w7x_regimes(surfaces, types, labels, magnetic_conf='',
+                     r_range=None, z_range=None, phi=np.nan, 
+                     boxes=None, aspect=False, save_image=False, legend=False):
+    """
+    Plot a Poincaré section of W7-X flux surfaces in the (R, z) plane.
+    Highlight different regimes based on the types array
+
+    Parameters
+    ----------
+    surfaces : list of FluxSurface or field-line-tracer result
+        Either a list of :class:`FluxSurface` instances (as returned by
+        :func:`load_w7x_flux_surfaces`) or a field-line-tracer result
+        object exposing ``poincare_res.surfs``.
+    types : np.array the shape of len(surfaces)
+        contains the type of each flux surfaces
+    labels : list of strings
+        contains the expression for each type - used in legend
+    magnetic_conf : str, optional
+        Name of the applied magnetic configuration, used in the title.
+    r_range, z_range : list of float, optional
+        Two-element ``[min, max]`` lists used to set the R and z axis
+        limits. If ``None`` (default), matplotlib chooses automatically.
+    s_range : optional
+        Reserved for future use.
+    phi : float, optional
+        Toroidal angle (in radians) at which the Poincaré section was
+        generated. Used in the title.
+    boxes : iterable of 4-tuples, optional
+        Box coordinates ``(x0, y0, x1, y1)`` to be overlaid on the plot.
+    aspect : bool, optional
+        If ``True``, use an equal aspect ratio. Otherwise, use the
+        matplotlib default.
+    save_image : bool or str, optional
+        If ``False`` (default), the figure is not saved to disk. Otherwise
+        the value is interpreted as the destination filename and the
+        figure is written as a PNG (a ``.png`` extension is appended if
+        missing).
+    """
+    if not isinstance(surfaces, list):
+        surfaces = surfaces.poincare_res.surfs
+
+    colors = ["red", "orange", "yellow", "green", "blue",
+              "purple", "indigo", "violet"]
+    fig, ax = plt.subplots()
+
+
+    for i, surface in enumerate(surfaces):
+        if surface.points.x1 is not None and len(surface.points.x1) > 0:
+            r = surface.points.r
+            z = surface.points.z
+            ax.scatter(r, z, color=colors[types[i]], s=0.2)
+        else:
+            print("Surface {} contains no points!".format(i + 1))
+    
+    for i in range(len(labels)): plt.scatter([0],[0], s=50, 
+                                   c=colors[i], label=labels[i])
+    ax.set_xlabel("R [m]")
+    ax.set_ylabel("z [m]")
+
+    if isinstance(r_range, list):
+        ax.set_xlim(r_range)
+
+    if isinstance(z_range, list):
+        ax.set_ylim(z_range)
+
+    if legend:
+        ax.legend()  
+
+    if aspect:
+        ax.set_aspect('equal')
+    ax.set_title(("Config: {}, toroidal angle: {:3.2f} rad, {:3.2f} deg."
+                  ).format(magnetic_conf, phi, phi/np.pi*180.))
+
+    if boxes:
+        for box in boxes:
+            r, z = box_plot_coordinates(box[0], box[1], box[2], box[3])
+            ax.plot(r, z)
+
+    if save_image:
+        save_filename = str(save_image)
+        if not save_filename.lower().endswith('.png'):
+            save_filename = save_filename + '.png'
+        fig.savefig(save_filename, format='png', dpi=300, bbox_inches="tight")
+
+    plt.show()
